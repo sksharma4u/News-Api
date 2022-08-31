f557e4dcd30758ed922eca0a095bfd6208de2720
sharmaji2612 <mahendrapal2612@gmail.com>
2022-09-01 02:34:05 +0530

    Table formation

--- news.js
+++ news.js
@@ -3,28 +3,30 @@
 function test() {
     fetch('https://newsapi.org/v2/top-headlines?country=in&apiKey=c648b1c626944ec3a1ec30fd6b886ea3')
         .then((response) => {
-            if (response.ok) {
-                return response.json()
+
+            if (response.ok) { //it will check that is there any data recieve from Api or not
+                console.log(response) //This data will be in json format
+                return response.json() //Now converted in the form of object and return it
             } else {
                 console.log(err)
             }
         })
-        .then((data) => {
+        .then((data) => { //having data that will be returned in the form object
             console.log(data.articles);
-            mynews(data.articles);
+            let tabledata = "";
+            data.articles.map((val) => {
+                tabledata += `
+                <tr>
+                <td>${val.author}</td>
+                    <td>${val.title}</td>
+                    <td>${val.description}</td>
+                    <td id = "hii"><img style = "width : 200px; height:250px" , src = "${val.urlToImage}"/></td>
+                    
+                </tr>`;
+            })
+            document.getElementById("mytable").innerHTML = tabledata;
+            // mynews(data.articles);
         })
-}
-
-function mynews(data) {
-    const ans = data;
-    const mydata = document.getElementById('mynews');
-    for (let i = 0; i < data.length; i++) {
-
-        const ele1 = document.createElement('h4');
-        ele1.innerText = `Author of the news : ${data[i].author} , Content : ${data[i].description}`;
-        mydata.append(ele1);
-    }
-    // mydata.innerHTML = JSON.stringify(ans);
 }
 
 test();
